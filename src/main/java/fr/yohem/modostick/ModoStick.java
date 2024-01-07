9ed0ed6fe1f3f84cf8b203ff29dc866c2d34b051
Yohem <vaxelaire.yohem@gmail.com>
2024-01-07 11:00:36 +0100

    armorStandStart

--- src/main/java/fr/yohem/modostick/ModoStick.java
+++ src/main/java/fr/yohem/modostick/ModoStick.java
@@ -2,14 +2,43 @@ package fr.yohem.modostick;
 
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public final class ModoStick extends JavaPlugin {
+    
+    Map<Player, Long>counterShowPlayer = new HashMap<Player, Long>(); 
+
+    static int timer = 3;
+
+    public void hidePlayerName(Player p){
+        ArmorStand armorStand = (ArmorStand) p.getWorld().spawnEntity(p.getLocation().add(0, 200, 0), EntityType.ARMOR_STAND);
+        armorStand.setCustomName("mask");
+        armorStand.setVisible(false);
+        p.addPassenger(armorStand);
+    }
+    public void showPlayerName(Player p){
+        int i =0;
+        while (i<p.getPassengers().size()) {
+            Entity e = p.getPassengers().get(i);
+            if (e.getType() == EntityType.ARMOR_STAND && ((ArmorStand)e).getCustomName().equals("mask")) {
+                p.removePassenger(e);
+            }else i++;
+        }
+        counterShowPlayer.put(p, System.currentTimeMillis());
+    }
 
     @Override
     public void onEnable() {
@@ -22,8 +51,28 @@ public final class ModoStick extends JavaPlugin {
                     }
                     return false;
                 });
-        getServer().getPluginManager().registerEvents(new ModoStickListerner(), this);
+        getServer().getPluginManager().registerEvents(new ModoStickListerner(this), this);
         // Plugin startup logic
+
+        for (Player player : getServer().getOnlinePlayers())
+            hidePlayerName(player);
+
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                Iterator<Entry<Player, Long>> iterator = counterShowPlayer.entrySet().iterator();
+                
+                while (iterator.hasNext()) {
+                    Entry<Player, Long> entry = iterator.next();
+                    if (System.currentTimeMillis() - entry.getValue()>= timer*1000) {
+                        hidePlayerName(entry.getKey());
+                        counterShowPlayer.remove(entry);
+                    }
+                }
+            }
+            
+        }.runTaskTimer(this, 0, 20L);
 
     }
     static public ItemStack getStick(){
--- src/main/java/fr/yohem/modostick/ModoStickListerner.java
+++ src/main/java/fr/yohem/modostick/ModoStickListerner.java
@@ -4,21 +4,29 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class ModoStickListerner implements Listener {
-    public ModoStickListerner (){
-
+    ModoStick modoStick;
+    public ModoStickListerner (ModoStick modoStick){
+        this.modoStick = modoStick;
     }
     @EventHandler
-    public void onJoin(PlayerInteractAtEntityEvent playerInteractEvent){
+    public void onIteract(PlayerInteractAtEntityEvent playerInteractEvent){
         Player p = playerInteractEvent.getPlayer();
-        if (/*playerInteractEvent.getRightClicked() instanceof Player &&*/ p.getInventory().getItemInMainHand().equals(ModoStick.getStick())){
-            if (p.hasPermission("stick.use"))
-                p.sendMessage( playerInteractEvent.getRightClicked().getName());
-            else{
+        if (playerInteractEvent.getRightClicked() instanceof Player && p.getInventory().getItemInMainHand().equals(ModoStick.getStick())){
+            if (p.hasPermission("stick.use")){
+                p.sendMessage(playerInteractEvent.getRightClicked().getName());
+                modoStick.hidePlayerName((Player)playerInteractEvent.getRightClicked());
+            }else{
                 p.sendMessage("Petit tricheur à la prochainne filouterie, la sentence risque d'être irrévocable");
                 p.getInventory().removeItem(ModoStick.getStick());
             }
         }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        modoStick.hidePlayerName(event.getPlayer());
     }
 }
